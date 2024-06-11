e2e2e30d7055ecf9791a0273e17bca3f93ac24c4
Paul Thompson <pmt10@case.edu>
2024-06-11 16:49:19 -0400

    add in source build test

---
+++ tests/in-source-build/project/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by wanglab on 6/11/2024.
+//
+
+
+#include "whiskertracker.hpp"
+
+#include <iostream>
+
+
+int main() {
+
+    auto wt = WhiskerTracker();
+
+    std::cout << "Whisker Tracker Created" << std::endl;
+
+
+    return 0;
+}
