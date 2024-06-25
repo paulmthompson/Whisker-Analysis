ecf88bb79178fa44f0b501f951c65258c04ca705
Paul Thompson <pmt10@case.edu>
2024-06-25 14:15:59 -0400

    update iwyu. add install command

--- tests/WhiskerTracking/test_data.hpp
+++ tests/WhiskerTracking/test_data.hpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include "whiskertracker.hpp"
 
@@ -12,22 +13,24 @@ std::string data_filename = "data/test_img.bin";
 inline const int img_width = 640;
 inline const int img_height = 480;
 
-
-
 inline auto load_img = [](std::string filename){
     std::ifstream stream(filename, std::ios::in | std::ios::binary);
 
-    std::vector<uint8_t> buffer;
-    buffer.resize(img_height * img_width);
+    std::vector<uint8_t> buffer(img_height * img_width);
     stream.read(reinterpret_cast<char*>(buffer.data()), img_height * img_width);
     stream.close();
     return buffer;
 };
 
-
 inline auto sample_img1 = load_img(data_filename);
 
-inline auto wt = whisker::WhiskerTracker();
+inline auto create_warmed_up_wt = []() {
+    auto wt = whisker::WhiskerTracker();
+    wt.setWhiskerPad(584.0f, 288.0f);
+
+    wt.trace(sample_img1, img_height, img_width);
+    return wt;
+};
 
 
 #endif //WHISKER_ANALYSIS_TEST_DATA_HPP
--- tests/WhiskerTracking/test_whiskertracker.cpp
+++ tests/WhiskerTracking/test_whiskertracker.cpp
@@ -4,9 +4,28 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
 
+#include <iostream>
+
 
 TEST_CASE("Test Trace", "[Trace]") {
 
-REQUIRE(sample_img1.size() == img_height * img_width);
+    static auto wt = create_warmed_up_wt();
+    static auto test_img = load_img("data/test_img.bin");
+
+    std::ifstream stream("data/test_img.bin", std::ios::in | std::ios::binary);
+
+    std::vector<uint8_t> buffer(img_height * img_width);
+    stream.read(reinterpret_cast<char*>(buffer.data()), img_height * img_width);
+    stream.close();
+
+    REQUIRE(test_img.size() == img_height * img_width);
+
+    std::cout << "The second index of test img is " << unsigned(test_img[1]) << std::endl;
+std::cout << "The second index of test img is " << unsigned(buffer[1]) << std::endl;
+
+    wt.trace(test_img, img_height, img_width);
+
+    REQUIRE(wt.whiskers.size() == 6);
 
 }
+
