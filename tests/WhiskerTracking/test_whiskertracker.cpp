85806e93f2d155e220a2c2cee310e8b95a462978
Paul Thompson <pmt10@case.edu>
2024-06-28 15:12:59 -0400

    add additional testing with multiple frames

--- tests/WhiskerTracking/test_whiskertracker.cpp
+++ tests/WhiskerTracking/test_whiskertracker.cpp
@@ -10,12 +10,22 @@
 TEST_CASE("Test Trace", "[Trace]") {
 
     static auto test_img = load_img("data/test_img.bin");
+    static auto test_img2 = load_img("data/test_img2.bin");
+    static auto test_img3 = load_img("data/test_img3.bin");
     static auto wt = create_warmed_up_wt(test_img);
 
     REQUIRE(test_img.size() == img_height * img_width);
 
     wt.trace(test_img, img_height, img_width);
 
+    REQUIRE(wt.whiskers.size() == 5);
+
+    wt.setWhiskerPad(576.0f, 273.0f);
+    wt.trace(test_img2, img_height, img_width);
+
+    REQUIRE(wt.whiskers.size() == 5);
+
+    wt.trace(test_img3, img_height, img_width);
     REQUIRE(wt.whiskers.size() == 5);
 
 }
