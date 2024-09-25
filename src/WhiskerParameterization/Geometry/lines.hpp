b958c46a81e10c95838ed2dafdef0eb21d66836e
Paul Thompson <pmt10@case.edu>
2024-09-25 13:01:04 -0400

    added curvature function with finite differences

--- src/WhiskerParameterization/Geometry/lines.hpp
+++ src/WhiskerParameterization/Geometry/lines.hpp
@@ -29,6 +29,64 @@ inline Line2D create_line(std::vector<float> const &x, std::vector<float> const 
 }
 
 /**
+ * @brief Generates a 2D line with a specified number of points between a start and end point.
+ *
+ * This function generates a 2D line by linearly interpolating between a start and end point.
+ * The number of points in the line is specified by the user. The points are evenly spaced along the line.
+ * If the number of points is less than 2, a line with only the start and end points is returned.
+ *
+ * @param start A Point2D<float> object representing the start point of the line.
+ * @param end A Point2D<float> object representing the end point of the line.
+ * @param num_points The number of points to be included in the line.
+ * @return A Line2D object representing the generated line. It is a vector of Point2D<float> objects.
+ */
+inline Line2D linspace(Point2D<float> start, Point2D<float> end, int const num_points)
+{
+    if (num_points < 2) {
+        std::cout << "Number of points must be greater than 1" << std::endl;
+        return Line2D{start, end};
+    }
+
+    Line2D line;
+    float dx = (end.x - start.x) / (num_points - 1);
+    float dy = (end.y - start.y) / (num_points - 1);
+    for (int i = 0; i < num_points; ++i) {
+        line.push_back({start.x + i * dx, start.y + i * dy});
+    }
+    return line;
+};
+
+/**
+ * @brief Generates a 2D line with points spaced at a specified distance between a start and end point.
+ *
+ * This function generates a 2D line by linearly interpolating between a start and end point.
+ * The spacing between points in the line is specified by the user. The points are evenly spaced along the line.
+ * If the total distance between the start and end points is less than the specified spacing, a line with only the start and end points is returned.
+ *
+ * @param start A Point2D<float> object representing the start point of the line.
+ * @param end A Point2D<float> object representing the end point of the line.
+ * @param spacing The distance between points to be included in the line.
+ * @return A Line2D object representing the generated line. It is a vector of Point2D<float> objects.
+ */
+inline Line2D linspace(Point2D<float> start, Point2D<float> end, float const spacing)
+{
+    auto total_distance = distance(start, end);
+
+    if (total_distance < spacing)
+    {
+        std::cout << "Spacing is greater than the total distance between the points" << std::endl;
+        return Line2D{start, end};
+    }
+
+    auto num_points = static_cast<int>(total_distance / spacing);
+
+    return linspace(start, end, num_points);
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+/**
  * @brief Calculates the total length of a 2D line.
  *
  * This function computes the total length of a 2D line by summing up the distances
@@ -46,6 +104,74 @@ inline float length(Line2D const &line) {
 
     return s;
 }
+
+/**
+ * @brief Finds the index of the point on a 2D line that is closest to a specified path length along the line.
+ *
+ * This function calculates the cumulative distance along a 2D line from the first point to each subsequent point.
+ * It stops when the cumulative distance exceeds the specified path length. The function then returns the index of the
+ * last point before the cumulative distance exceeded the path length, and the cumulative distance up to that point.
+ *
+ * @param line A Line2D object representing the 2D line. It is a vector of Point2D<float> objects.
+ * @param pathlength The path length along the line for which the nearest preceding point is to be found.
+ * @return A tuple containing the index of the nearest preceding point and the cumulative distance up to that point.
+ */
+inline std::tuple<int, float> nearest_preceding_index_along_path(Line2D const &line, float const pathlength) {
+    if (line.size() < 2) {
+        std::cout << "Line does not contain at least 2 points" << std::endl;
+        return std::make_tuple(0, 0.0f);
+    } else if (length(line) < pathlength) {
+        std::cout << "The requested pathlength is greater than the length of the line" << std::endl;
+        return std::make_tuple(0, 0.0f);
+    }
+
+    float s = 0.0f;
+    int closest_pre_ind = 0;
+
+    for (std::size_t i = 1; i < line.size(); i++) {
+        auto dist = distance(line[i], line[i - 1]);
+        if (s + dist > pathlength) {
+            break;
+        } else {
+            s = s + dist;
+            closest_pre_ind += 1;
+        }
+    }
+
+    return std::make_tuple(closest_pre_ind, s);
+}
+
+/**
+ * @brief Finds the point on a 2D line that is closest to a specified path length along the line.
+ *
+ * This function calculates the cumulative distance along a 2D line from the first point to each subsequent point.
+ * It stops when the cumulative distance exceeds the specified path length. The function then returns the point on the
+ * line that is at the specified path length from the start of the line.
+ * It linearly interpolates between the two points that are closest to the specified path length to find the point.
+ *
+ * @param line A Line2D object representing the 2D line. It is a vector of Point2D<float> objects.
+ * @param pathlength The path length along the line for which the point is to be found.
+ * @return A Point2D<float> object representing the point on the line that is at the specified path length from the start of the line.
+ */
+inline Point2D<float> point_at_pathlength(Line2D const &line, float const pathlength) {
+    if (line.size() < 2) {
+        std::cout << "Line does not contain at least 2 points" << std::endl;
+        return Point2D<float>{0.0f, 0.0f};
+    } else if (length(line) < pathlength) {
+        std::cout << "The requested pathlength is greater than the length of the line" << std::endl;
+        return line.back();
+    }
+
+    auto const [closest_pre_ind, dist] = nearest_preceding_index_along_path(line, pathlength);
+
+    //auto final_segment_dist = distance(line[closest_pre_ind],line[closest_pre_ind+1]);
+
+    return point_along_path(line[closest_pre_ind], line[closest_pre_ind + 1], pathlength - dist);
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
 
 /**
  * @brief Calculates the minimum distance from a point to a 2D line.
@@ -144,124 +270,8 @@ inline float minimum_distance_exact(Line2D const & line, Point2D<float> const p)
     return min_dist;
 }
 
-/**
- * @brief Finds the index of the point on a 2D line that is closest to a specified path length along the line.
- *
- * This function calculates the cumulative distance along a 2D line from the first point to each subsequent point.
- * It stops when the cumulative distance exceeds the specified path length. The function then returns the index of the
- * last point before the cumulative distance exceeded the path length, and the cumulative distance up to that point.
- *
- * @param line A Line2D object representing the 2D line. It is a vector of Point2D<float> objects.
- * @param pathlength The path length along the line for which the nearest preceding point is to be found.
- * @return A tuple containing the index of the nearest preceding point and the cumulative distance up to that point.
- */
-inline std::tuple<int, float> nearest_preceding_index_along_path(Line2D const &line, float const pathlength) {
-    if (line.size() < 2) {
-        std::cout << "Line does not contain at least 2 points" << std::endl;
-        return std::make_tuple(0, 0.0f);
-    } else if (length(line) < pathlength) {
-        std::cout << "The requested pathlength is greater than the length of the line" << std::endl;
-        return std::make_tuple(0, 0.0f);
-    }
+///////////////////////////////////////////////////////////////////////////////
 
-    float s = 0.0f;
-    int closest_pre_ind = 0;
-
-    for (std::size_t i = 1; i < line.size(); i++) {
-        auto dist = distance(line[i], line[i - 1]);
-        if (s + dist > pathlength) {
-            break;
-        } else {
-            s = s + dist;
-            closest_pre_ind += 1;
-        }
-    }
-
-    return std::make_tuple(closest_pre_ind, s);
-}
-
-/**
- * @brief Finds the point on a 2D line that is closest to a specified path length along the line.
- *
- * This function calculates the cumulative distance along a 2D line from the first point to each subsequent point.
- * It stops when the cumulative distance exceeds the specified path length. The function then returns the point on the
- * line that is at the specified path length from the start of the line.
- * It linearly interpolates between the two points that are closest to the specified path length to find the point.
- *
- * @param line A Line2D object representing the 2D line. It is a vector of Point2D<float> objects.
- * @param pathlength The path length along the line for which the point is to be found.
- * @return A Point2D<float> object representing the point on the line that is at the specified path length from the start of the line.
- */
-inline Point2D<float> point_at_pathlength(Line2D const &line, float const pathlength) {
-    if (line.size() < 2) {
-        std::cout << "Line does not contain at least 2 points" << std::endl;
-        return Point2D<float>{0.0f, 0.0f};
-    } else if (length(line) < pathlength) {
-        std::cout << "The requested pathlength is greater than the length of the line" << std::endl;
-        return line.back();
-    }
-
-    auto const [closest_pre_ind, dist] = nearest_preceding_index_along_path(line, pathlength);
-
-    //auto final_segment_dist = distance(line[closest_pre_ind],line[closest_pre_ind+1]);
-
-    return point_along_path(line[closest_pre_ind], line[closest_pre_ind + 1], pathlength - dist);
-}
-
-/**
- * @brief Generates a 2D line with a specified number of points between a start and end point.
- *
- * This function generates a 2D line by linearly interpolating between a start and end point.
- * The number of points in the line is specified by the user. The points are evenly spaced along the line.
- * If the number of points is less than 2, a line with only the start and end points is returned.
- *
- * @param start A Point2D<float> object representing the start point of the line.
- * @param end A Point2D<float> object representing the end point of the line.
- * @param num_points The number of points to be included in the line.
- * @return A Line2D object representing the generated line. It is a vector of Point2D<float> objects.
- */
-inline Line2D linspace(Point2D<float> start, Point2D<float> end, int const num_points)
-{
-    if (num_points < 2) {
-        std::cout << "Number of points must be greater than 1" << std::endl;
-        return Line2D{start, end};
-    }
-
-    Line2D line;
-    float dx = (end.x - start.x) / (num_points - 1);
-    float dy = (end.y - start.y) / (num_points - 1);
-    for (int i = 0; i < num_points; ++i) {
-        line.push_back({start.x + i * dx, start.y + i * dy});
-    }
-    return line;
-};
-
-/**
- * @brief Generates a 2D line with points spaced at a specified distance between a start and end point.
- *
- * This function generates a 2D line by linearly interpolating between a start and end point.
- * The spacing between points in the line is specified by the user. The points are evenly spaced along the line.
- * If the total distance between the start and end points is less than the specified spacing, a line with only the start and end points is returned.
- *
- * @param start A Point2D<float> object representing the start point of the line.
- * @param end A Point2D<float> object representing the end point of the line.
- * @param spacing The distance between points to be included in the line.
- * @return A Line2D object representing the generated line. It is a vector of Point2D<float> objects.
- */
-inline Line2D linspace(Point2D<float> start, Point2D<float> end, float const spacing)
-{
-    auto total_distance = distance(start, end);
-
-    if (total_distance < spacing)
-    {
-        std::cout << "Spacing is greater than the total distance between the points" << std::endl;
-        return Line2D{start, end};
-    }
-
-    auto num_points = static_cast<int>(total_distance / spacing);
-
-    return linspace(start, end, num_points);
-};
 
 inline void unit_linear_extend_base(Line2D &line, int vec_index = 5)
 {
---
+++ tests/WhiskerParameterization/Geometry/test_curvature.cpp
@@ -0,0 +1,95 @@
+
+#include "Geometry/lines.hpp"
+#include "Geometry/curvature.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include <string>
+
+/**
+ * 
+ * A parabola has defined curvature at every point according to the curve:
+ * 
+ * k(x) = 2 * a / (1 + (2*a*x + b)^2)^(3/2)
+ * 
+ * 
+ */
+TEST_CASE("FiniteDifferences - 1", "[Curvature]") {
+
+    const float a = 1.0f;
+    const float b = 0.0f;
+    const float c = 0.0f;
+
+    //Generate a parabola with a = 1, b = 0 over the range -10 to 10 with 100 points
+    auto x = std::vector<float>(100);
+    auto y = std::vector<float>(100);
+
+    for (int i = 0; i < 100; i++) {
+        x[i] = -10.0f + 0.2f * i;
+        y[i] = a * x[i] * x[i] + b * x[i] + c;
+    }
+
+    auto line = whisker::create_line(x, y);
+
+    auto index = 50;
+
+    // (0.04 - 0.04) / (0.2 * 2) = 0 / 0.4 = 0.0
+
+    auto fd = whisker::central_difference_first_derivative(line, index, 1);
+
+    CHECK_THAT(fd, Catch::Matchers::WithinAbs(0.0f, 1e-6));
+
+    // Check that this still works when the window size is increased
+    fd = whisker::central_difference_first_derivative(line, index, 2);
+
+    CHECK_THAT(fd, Catch::Matchers::WithinAbs(0.0f, 1e-6));
+
+    // Create new line with index 51 deleted so that it is asymmetric
+    x.erase(x.begin() + 51);
+    y.erase(y.begin() + 51);
+
+    line = whisker::create_line(x, y);
+
+    // (x = -0.2, y = 0.04)
+    // (x = 0.4, y = 0.16)
+    // (0.16 - 0.04) / (0.6) = 0.12 / 0.6 = 0.2
+
+    fd = whisker::central_difference_first_derivative(line, index, 1);
+
+    CHECK_THAT(static_cast<double>(fd), Catch::Matchers::WithinRel(0.2, 1e-3));
+}
+
+TEST_CASE("Curvature - 1", "[Curvature]") {
+
+    const float a = 1.0f;
+    const float b = 0.0f;
+    const float c = 0.0f;
+
+    //Generate a parabola with a = 1, b = 0 over the range -10 to 10 with 100 points
+    auto x = std::vector<float>(100);
+    auto y = std::vector<float>(100);
+
+    for (int i = 0; i < 100; i++) {
+        x[i] = -10.0f + 0.2f * i;
+        y[i] = a * x[i] * x[i] + b * x[i] + c;
+    }
+
+    auto line = whisker::create_line(x, y);
+
+    auto index = 50;
+
+    // (2 * a) / (1 + (2 * a * x + b)^2)^(3/2)
+    // (2 * 1) / (1 + (2 * 1 * 0 + 0)^2)^(3/2) = 2 / 1 = 2
+
+    auto curvature = whisker::calculate_curvature_finite_differences(line, index, 1);
+
+    CHECK_THAT(curvature, Catch::Matchers::WithinRel(2.0f, 1e-3));
+
+    // Check that this still works when the window size is increased
+    curvature = whisker::calculate_curvature_finite_differences(line, index, 2);
+
+    CHECK_THAT(curvature, Catch::Matchers::WithinRel(2.0f, 1e-3));
+
+}
