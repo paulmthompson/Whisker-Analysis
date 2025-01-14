bd23ac35a447c0d512f6ac0b4b52df334083b591
github-action-benchmark <github@users.noreply.github.com>
2025-01-14 19:47:59 +0000

    add Benchmark (catch2) benchmark result for 264d118592213e97aa90c3e26571117dd4b1c00f

--- docs/dev/bench/data.js
+++ docs/dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736883301702,
+  "lastUpdate": 1736884079057,
   "repoUrl": "https://github.com/paulmthompson/Whisker-Analysis",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,72 @@ window.BENCHMARK_DATA = {
             "range": "± 4.70079",
             "unit": "ns",
             "extra": "100 samples\n1033 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pmt10@case.edu",
+            "name": "Paul Thompson",
+            "username": "paulmthompson"
+          },
+          "committer": {
+            "email": "pmt10@case.edu",
+            "name": "Paul Thompson",
+            "username": "paulmthompson"
+          },
+          "distinct": true,
+          "id": "264d118592213e97aa90c3e26571117dd4b1c00f",
+          "message": "add line benchmarks",
+          "timestamp": "2025-01-14T14:46:36-05:00",
+          "tree_id": "0859862830ce38d974ee5888f09cc47e55b13093",
+          "url": "https://github.com/paulmthompson/Whisker-Analysis/commit/264d118592213e97aa90c3e26571117dd4b1c00f"
+        },
+        "date": 1736884078578,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "IoU Calculation",
+            "value": 7.90806,
+            "range": "± 489.939",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          },
+          {
+            "name": "IoU Relative Calculation",
+            "value": 7.24077,
+            "range": "± 382.635",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
+          },
+          {
+            "name": "Mask Intersection",
+            "value": 29.8612,
+            "range": "± 2.43715",
+            "unit": "ns",
+            "extra": "100 samples\n1044 iterations"
+          },
+          {
+            "name": "length Calculation",
+            "value": 277.806,
+            "range": "± 0.494422",
+            "unit": "ns",
+            "extra": "100 samples\n107 iterations"
+          },
+          {
+            "name": "Nearest Preceding Calculation",
+            "value": 532.821,
+            "range": "± 14.4062",
+            "unit": "ns",
+            "extra": "100 samples\n54 iterations"
+          },
+          {
+            "name": "Nearest Interpolated Calculation",
+            "value": 844.296,
+            "range": "± 23.256",
+            "unit": "ns",
+            "extra": "100 samples\n36 iterations"
           }
         ]
       }
