7f0d901b271ec24859261b471a56bf2d5758c659
Paul Thompson <pmt10@case.edu>
2025-01-15 14:22:01 -0500

    fix name of benchmark test

--- tests/WhiskerTracking/benchmark_whiskertracker.cpp
+++ tests/WhiskerTracking/benchmark_whiskertracker.cpp
@@ -7,7 +7,7 @@
 
 #include "test_data.hpp"
 
-TEST_CASE("Test Trace", "[Trace]") {
+TEST_CASE("Benchmark Trace", "[BENCHMARK]") {
 
     static auto test_img = load_img("../tests/WhiskerTracking/data/0110_1_0000000.bin");
     static auto wt = create_warmed_up_wt(test_img);
